f119556daade95751008aa20c6b105c50a0db8a0
Escobar Correas <esc006@csiro.au>
2020-03-17 11:19:42 +1000

    select,separate,group_by

--- exercise_2.R
+++ exercise_2.R
@@ -5,6 +5,11 @@ BOM_stations <- read_csv('data/BOM_stations.csv')
 BOM_data
 BOM_stations
 select(BOM_data, Station_number,Temp_min_max)
-Bom_data_temp <- select(BOM_data, Station_number,Temp_min_max, Rainfall) %>% 
-  separate(Temp_min_max,into= c('min', 'max'), sep='/')
+Bom_data_temp <- select(BOM_data, Station_number,Temp_min_max, Rainfall, Day) %>% 
+  separate(Temp_min_max,into= c('min', 'max'), sep='/') %>% filter(min!= '-', max!= '-',
+                                                                   Rainfall> 0)
 Bom_data_temp
+
+Bom_data_minmax <-group_by(Bom_data_temp, Station_number)
+Bom_data_minmax
+
