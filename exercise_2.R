447169460683d2a92fbde56b0a0473b7f78526e9
Escobar Correas <esc006@csiro.au>
2020-03-17 14:09:03 +1000

    quest2

--- exercise_2.R
+++ exercise_2.R
@@ -16,6 +16,14 @@ Bom_data_minmax <-group_by(Bom_data_temp, Station_number) %>%
 Bom_data_minmax
 
 #Question 2
+Bom_data_temp <- select(BOM_data, Station_number,Month,Temp_min_max, Rainfall) %>% 
+  filter(Temp_min_max!= '-/-', Rainfall != '-') %>% 
+  separate(Temp_min_max,into= c('min', 'max'), sep='/') 
+
 Bom_data_temp
-mutate (Bom_data_temp, dif_temp= 'max' - 'min')
-?mutate
+Bom_data_temp$min<-as.numeric(Bom_data_temp$min)
+Bom_data_temp$max<-as.numeric(Bom_data_temp$max)
+mutate (Bom_data_temp, dif_temp= max - min) %>% group_by(Month) %>% 
+  summarise(n_dif_temp= mean(dif_temp))
+Bom_data_temp
+                                                                              
